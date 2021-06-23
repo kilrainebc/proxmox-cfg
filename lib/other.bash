8446dde9eb96a262403610e9ab61c4d18f5b2798
kilraine.b.c <35840407+kilrainebc@users.noreply.github.com>
2021-06-23 15:53:44 -0400

    Added remove functions to to other.bash library

--- lib/other.bash
+++ lib/other.bash
@@ -25,6 +25,7 @@ function setup_pmx_noreminder () {
   echo "$file"
   printf "/usr/share/javascript/proxmox-widget-toolkit/ IN_CREATE /usr/local/bin/%s \$#\n" "$file" >> /tmp/incron.table-temp
   incrontab -u root /tmp/incron.table-temp
+  rm -f /tmp/incron.table-temp
 
   tail /var/log/syslog | grep incrond
   tail -n 30 /var/log/incron.log
@@ -33,11 +34,51 @@ function setup_pmx_noreminder () {
 
 #######################################
 # Description:
+#   Applies the standard no-reminder warning.
+#######################################
+function remove_pmx_noreminder () {
+  printf "root" >> /etc/incron.allow
+  local file
+  file="/usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js"
+  cp "$file.bkp" "$file" 
+  systemctl restart pveproxy.service
+
+  incrontab -r
+  apt-get install --reinstall proxmox-widget-toolkit  
+}
+
+#######################################
+# Description:
 #   Applies dark theme for Proxmox webGUI.
 #######################################
 function setup_pmx_darkmode () {
-  wget https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.sh
-  bash PVEDiscordDark.sh install
+  local file
+  file="PVEDiscordDark.sh"
+  if [[ ! -f $file ]]; then
+    wget https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.sh
+  fi
+  bash $file install
+}
+
+#######################################
+# Description:
+#   Removes dark theme for Proxmox webGUI.
+#######################################
+function remove_pmx_darkmode () {
+  local file
+  file="PVEDiscordDark.sh"
+  if [[ -f $file ]]; then
+    bash $file uninstall
+  fi
+}
+
+#######################################
+# Description:
+#   Reverses changes made by other_config function
+#######################################
+function remove_other_config () {
+  remove_pmx_darkmode
+  remove_pmx_noreminder
 }
 
 #######################################
@@ -48,3 +89,4 @@ function other_config () {
   setup_pmx_darkmode
   setup_pmx_noreminder
 }
+
